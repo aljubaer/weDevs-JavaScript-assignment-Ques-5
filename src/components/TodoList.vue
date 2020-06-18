2593cefd797b9cc7dd7867bd550332f0cd4a745f
Abdullah Al Jubaer <bsse0812@iit.du.ac.bd>
2020-06-18 15:49:12 +0600

    functions are done. todo: use vuex

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,15 +7,15 @@
       v-model="newTodo"
       @keyup.enter="addTodo"
     />
-    <div class="todo-item" v-for="(todo, index) in todos" :key="todo.id">
+    <div class="todo-item" v-for="(todo, index) in filteredTodos" :key="todo.id">
       <div class="todo-item-left">
+        <input type="checkbox" v-model="todo.completed" />
         <div
           v-if="!todo.onEditing"
           @dblclick="editTodo(todo)"
           class="todo-item-label"
-        >
-          {{ todo.title }}
-        </div>
+          v-bind:class="{ todoCompleted: todo.completed }"
+        >{{ todo.title }}</div>
         <input
           v-else
           type="text"
@@ -23,12 +23,32 @@
           v-model="todo.title"
           @blur="onFinishEdit(todo)"
           @keyup.enter="onFinishEdit(todo)"
+          @keyup.esc="onCancelEdit(todo)"
           v-focus
         />
       </div>
-      <div class="remove-item" @click="removeTodo(index)">
-        &times;
+      <div class="remove-item" @click="removeTodo(index)">&times;</div>
+    </div>
+    <div class="extra-container">
+      <div>
+        <label>
+          <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos" />
+          Check All
+        </label>
       </div>
+
+      <div>
+        <button v-bind:class="{ active: filter == 'all' }" @click="filter = 'all'">All</button>
+        <button v-bind:class="{ active: filter == 'active' }" @click="filter = 'active'">Active</button>
+        <button
+          v-bind:class="{ active: filter == 'completed' }"
+          @click="filter = 'completed'"
+        >Completed</button>
+      </div>
+
+      <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+
+      <div>{{remainingTodos}} items left.</div>
     </div>
   </div>
 </template>
@@ -40,7 +60,9 @@ export default {
     return {
       newTodo: "",
       nextId: 1,
-      todos: []
+      todos: [],
+      beforeEdit: "",
+      filter: "all"
     };
   },
   directives: {
@@ -50,8 +72,26 @@ export default {
       }
     }
   },
+  computed: {
+    remainingTodos() {
+      return this.todos.filter(todo => !todo.completed).length;
+    },
+    anyRemaining() {
+      return this.remainingTodos != 0;
+    },
+    filteredTodos() {
+      if (this.filter == "all") return this.todos;
+      else if (this.filter == "completed")
+        return this.todos.filter(todo => todo.completed == true);
+      else return this.todos.filter(todo => todo.completed == false);
+    },
+    showClearCompletedButton() {
+      return this.todos.filter(todo => todo.completed == true).length > 0;
+    }
+  },
   methods: {
     addTodo() {
+      console.log("called");
       if (this.newTodo.trim().length == 0) return;
 
       this.todos.push({
@@ -65,13 +105,29 @@ export default {
       this.nextId++;
     },
     editTodo(todo) {
+      this.beforeEdit = todo.title;
       todo.onEditing = true;
     },
     onFinishEdit(todo) {
+      if (todo.title.trim().length == 0) {
+        todo.title = this.beforeEdit;
+      }
+      todo.onEditing = false;
+    },
+    onCancelEdit(todo) {
+      todo.title = this.beforeEdit;
       todo.onEditing = false;
     },
     removeTodo(index) {
       this.todos.splice(index, 1);
+    },
+    checkAllTodos() {
+      this.todos.map(todo => {
+        todo.completed = event.target.checked;
+      });
+    },
+    clearCompleted() {
+      this.todos = this.todos.filter(todo => todo.completed == false);
     }
   }
 };
@@ -127,5 +183,37 @@ export default {
   &:focus {
     outline: none;
   }
+}
+
+.todoCompleted {
+  color: gray;
+}
+
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  border-top: 1px solid lightgray;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+
+button {
+  font-size: 14px;
+  background-color: white;
+  appearance: none;
+  outline: none;
+
+  &:hover {
+    background: lightgreen;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+.active {
+  background: lightgreen;
 }
 </style>
